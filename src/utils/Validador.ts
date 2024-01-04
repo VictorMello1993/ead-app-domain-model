8176f8ca05eba0bde6e4689cd02e1036e6c793bb
Victor Mello <victorsmello93@gmail.com>
2024-01-03 22:29:59 -0300

    Validador (parte 2)

--- src/utils/Validador.ts
+++ src/utils/Validador.ts
@@ -1,7 +1,32 @@
 import { ErroValidacao } from "@/errors/ErroValidacao";
 
 export class Validador {
+  static combinar(...erros: (ErroValidacao | null)[]): ErroValidacao[] | null {
+    const errosFiltrados = erros.filter(erro => erro !== null) as ErroValidacao[];
+    return errosFiltrados.length > 0 ? errosFiltrados : null;
+  }
+
   static naoNulo(valor: any, erro: string): ErroValidacao | null {
     return valor !== null && valor !== undefined ? null : ErroValidacao.novo(erro, valor);
+  }
+
+  static naoVazio(valor: string | null | undefined, erro: string) {
+    if (Validador.naoNulo(valor, erro)) {
+      return ErroValidacao.novo(erro, valor);
+    }
+
+    return valor!.trim() !== "" ? null : ErroValidacao.novo(erro, valor);
+  }
+
+  static tamanhoMenorQue(valor: string | any [], tamanhoMaximo: number, erro: string): ErroValidacao | null {
+    return valor.length < tamanhoMaximo ? null : ErroValidacao.novo(erro, valor, { max: tamanhoMaximo });
+  }
+
+  static tamanhoMaiorQue(valor: string | any [], tamanhoMinimo: number, erro: string): ErroValidacao | null {
+    return valor.length > tamanhoMinimo ? null : ErroValidacao.novo(erro, valor, { min: tamanhoMinimo });
+  }
+
+  static regex(valor: string, regex: RegExp, erro: string): ErroValidacao | null {
+    return regex.test(valor) ? null : ErroValidacao.novo(erro, valor);
   }
 }
--- tests/utils/Validador.spec.ts
+++ tests/utils/Validador.spec.ts
@@ -10,3 +10,81 @@ test("Deve retornar erro com texto nulo", () => {
   const erro = Validador.naoNulo(null, msgErro);
   expect(erro?.codigo).toBe(msgErro);
 });
+
+test("Deve retornar erro com texto nulo", () => {
+  const msgErro = "Texto inválido";
+  const erro = Validador.naoNulo(null, msgErro);
+  expect(erro?.codigo).toBe(msgErro);
+});
+
+test("Deve retornar null com texto não vazio", () => {
+  const erro = Validador.naoVazio("ABC", "Texto vazio");
+  expect(erro).toBeNull();
+});
+
+test("Deve retornar erro com texto vazio e/ou nulo", () => {
+  const msgErro = "Texto inválido";
+  const erro1 = Validador.naoVazio("            ", msgErro);
+  const erro2 = Validador.naoVazio(null, msgErro);
+  expect(erro1?.codigo).toBe(msgErro);
+  expect(erro2?.codigo).toBe(msgErro);
+});
+
+test("Deve retornar erro com texto undefined", () => {
+  const msgErro = "Texto inválido";
+  const erro1 = Validador.naoVazio(undefined, msgErro);
+  expect(erro1?.codigo).toBe(msgErro);
+});
+
+test("Deve retornar null com texto menor que o tamanho máximo", () => {
+  const erro = Validador.tamanhoMenorQue("teste", 6, "erro");
+  expect(erro).toBeNull();
+});
+
+test("Deve retornar erro com texto maior que o tamanho máximo", () => {
+  const erro = Validador.tamanhoMenorQue("Bom dia", 6, "erro");
+  expect(erro?.codigo).toBe("erro");
+});
+
+test("Deve retornar null com texto maior que o tamanho mínimo", () => {
+  const erro = Validador.tamanhoMaiorQue("teste", 4, "erro");
+  expect(erro).toBeNull();
+});
+
+test("Deve retornar erro com texto menor que o tamanho mínimo", () => {
+  const erro = Validador.tamanhoMaiorQue("bom", 4, "erro");
+  expect(erro?.codigo).toBe("erro");
+});
+
+test("Deve validar regex que só tem números", () => {
+  const erro = Validador.regex("1234567890", /\d{10}/, "erro");
+  expect(erro).toBeNull();
+});
+
+test("Deve retornar erro na validação de números", () => {
+  const erro = Validador.regex("123456f7890", /\d{10}/, "erro");
+  expect(erro?.codigo).toBe("erro");
+});
+
+test("Deve combinar os erros", () => {
+  const erros = Validador.combinar(
+    Validador.naoVazio("", "erro1"),
+    Validador.naoVazio("", "erro2"),
+    Validador.naoVazio("", "erro3"),
+    Validador.naoVazio("teste", "nao erro 4"),
+    Validador.naoVazio("", "erro5")
+  );
+
+  expect(erros?.map(erro => erro.codigo).join(", ")).toBe("erro1, erro2, erro3, erro5");
+});
+
+test("Deve combinar sem erros", () => {
+  const erros = Validador.combinar(
+    Validador.naoVazio("teste1", "nao erro 1"),
+    Validador.naoVazio("teste2", "nao erro 2"),
+    Validador.naoVazio("teste3", "nao erro 3"),
+    Validador.naoVazio("teste4", "nao erro 4")
+  );
+
+  expect(erros).toBeNull();
+});
