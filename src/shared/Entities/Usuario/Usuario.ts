28179c7623189105f10e534ddc4271315994f062
VictorMello1993 <victorsmello93@gmail.com>
2024-01-07 22:16:26 -0300

    refactor: adicionando a propriedade senha na entidade de usuário

--- src/shared/Entities/Usuario/Usuario.ts
+++ src/shared/Entities/Usuario/Usuario.ts
@@ -1,15 +1,26 @@
 import { Entidade, EntidadeProps } from "@/shared/Entities/Entidade";
 import { NomePessoaVO } from "@/shared/ValueObject/NomePessoaVO";
+import { EmailVO } from "@/shared/ValueObject/EmailVO";
+import SenhaVO from "@/shared/ValueObject/SenhaVO";
 
 export interface UsuarioProps extends EntidadeProps{
   nome?: string
+  email?: string
+  senha?: string
 }
 
 export class Usuario extends Entidade<Usuario, UsuarioProps> {
   readonly nome: NomePessoaVO;
+  readonly email: EmailVO;
+  readonly senha?: SenhaVO;
 
   constructor(props: UsuarioProps) {
     super(props);
     this.nome = new NomePessoaVO(props.nome);
+    this.email = new EmailVO(props.email);
+
+    if (props.senha) {
+      this.senha = new SenhaVO(props.senha);
+    }
   }
 }
--- src/shared/ValueObject/SenhaVO.ts
+++ src/shared/ValueObject/SenhaVO.ts
@@ -4,8 +4,8 @@ import { ErroValidacao } from "@/errors/ErroValidacao";
 export default class SenhaVO {
   static readonly SENHA_FORTE_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 
-  constructor(readonly valor: string) {
-    if (!SenhaVO.isValida(valor)) {
+  constructor(readonly valor?: string) {
+    if (!SenhaVO.isValida(valor ?? "")) {
       ErroValidacao.lancar(Erros.SENHA_FRACA);
     }
   }
